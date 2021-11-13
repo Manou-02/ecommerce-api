d980cd8a8978b641d1ffe96f67ac7bce79e51326
Manou-02 <mrakotoarivelo97@gmail.com>
2021-11-13 06:54:18 +0100

    Commande delete & getOne

--- src/controllers/commande.controller.js
+++ src/controllers/commande.controller.js
@@ -1,4 +1,5 @@
 const Commande = require('../models/commande');
+const ObjectId = require('mongoose').Types.ObjectId;
 
 /**
  * Get all commande
@@ -37,6 +38,19 @@ module.exports.createCommande = async (req, res, next) => {
  * @param {*} next 
  */
 module.exports.getOneCommande = async (req, res, next) => {
+    const {id} = req.params;
+    if(!ObjectId.isValid(id)) return res.status(404).json({erreur : "ID inconnue"})
+    
+    try{
+        await Commande.findOne({_id : id}).then(docs => {
+            res.status(200).json({commande : docs})
+        }).catch(err => {
+            console.log(err);
+            res.status(400).json({erreur : err})
+        })
+    }catch(err){
+        console.log(err);
+    }
 
 }
 
@@ -57,7 +71,19 @@ module.exports.updateCommade = async (req, res, next) => {
  * @param {*} next 
  */
 module.exports.deleteCommande = async (req, res, next) => {
+    const {id} = req.params;
+    if(!ObjectId.isValid(id)) return res.status(404).json({erreur : "ID inconnue"})
 
+    try{
+        await Commande.deleteOne({_id : id}).then(() => {
+            res.status(200).json({success : "Commande supprimé"});
+        }).catch(err => {
+            console.log(err);
+            res.status(400).json({erreur : "Erreur lors de la suppression"})
+        })
+    }catch(err){
+        console.log(err);
+    }
 }
 
 
